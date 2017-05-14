7a05d995c7b9d2d1d8291921b323a27e12aee7b3
Manojkumar <manojkumar99tech@gmail.com>
2017-05-14 17:00:12 +0800

    pin intialised & imported to STM Workbench IDE

---
+++ Debug/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.d
@@ -0,0 +1,77 @@
+Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.o: \
+ ../Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.c \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Inc/stm32l0xx_hal_conf.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Inc/main.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cm0plus.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ /home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Inc/stm32l0xx_hal_conf.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Inc/main.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cm0plus.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+/home/manojkumar/STM_Projects/STM32L053_Nucleo/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
